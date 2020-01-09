9f809c34d2d6f8a0e0ef60e5ef3d3c52fd473612
linlijian <praw__Rul2__lvluk@Outlook.co.th>
2020-01-09 15:30:11 +0700

    MENU [color, icon]

--- WFA-MangaHonKo/Form1.Designer.cs
+++ WFA-MangaHonKo/Form1.Designer.cs
@@ -33,10 +33,12 @@
             this.lblMangaHonKo = new Bunifu.Framework.UI.BunifuCustomLabel();
             this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
             this.panelLeft = new System.Windows.Forms.Panel();
+            this.btnCatalogs = new Bunifu.Framework.UI.BunifuFlatButton();
             this.btnHome = new Bunifu.Framework.UI.BunifuFlatButton();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
             this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.btnUpdates = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnFavorites = new Bunifu.Framework.UI.BunifuFlatButton();
             this.panelTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.panelLeft.SuspendLayout();
@@ -82,7 +84,9 @@
             // panelLeft
             // 
             this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(16)))), ((int)(((byte)(19)))));
-            this.panelLeft.Controls.Add(this.bunifuFlatButton1);
+            this.panelLeft.Controls.Add(this.btnFavorites);
+            this.panelLeft.Controls.Add(this.btnUpdates);
+            this.panelLeft.Controls.Add(this.btnCatalogs);
             this.panelLeft.Controls.Add(this.btnHome);
             this.panelLeft.Controls.Add(this.panel1);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
@@ -91,10 +95,47 @@
             this.panelLeft.Size = new System.Drawing.Size(253, 693);
             this.panelLeft.TabIndex = 2;
             // 
+            // btnCatalogs
+            // 
+            this.btnCatalogs.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(125)))), ((int)(((byte)(154)))));
+            this.btnCatalogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnCatalogs.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCatalogs.BorderRadius = 0;
+            this.btnCatalogs.ButtonText = "Catalogs";
+            this.btnCatalogs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCatalogs.DisabledColor = System.Drawing.Color.Gray;
+            this.btnCatalogs.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCatalogs.Font = new System.Drawing.Font("Itim", 12F);
+            this.btnCatalogs.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnCatalogs.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnCatalogs.Iconimage")));
+            this.btnCatalogs.Iconimage_right = null;
+            this.btnCatalogs.Iconimage_right_Selected = null;
+            this.btnCatalogs.Iconimage_Selected = null;
+            this.btnCatalogs.IconMarginLeft = 30;
+            this.btnCatalogs.IconMarginRight = 0;
+            this.btnCatalogs.IconRightVisible = true;
+            this.btnCatalogs.IconRightZoom = 0D;
+            this.btnCatalogs.IconVisible = true;
+            this.btnCatalogs.IconZoom = 50D;
+            this.btnCatalogs.IsTab = true;
+            this.btnCatalogs.Location = new System.Drawing.Point(0, 107);
+            this.btnCatalogs.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnCatalogs.Name = "btnCatalogs";
+            this.btnCatalogs.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnCatalogs.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.btnCatalogs.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnCatalogs.selected = false;
+            this.btnCatalogs.Size = new System.Drawing.Size(253, 47);
+            this.btnCatalogs.TabIndex = 6;
+            this.btnCatalogs.Text = "Catalogs";
+            this.btnCatalogs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnCatalogs.Textcolor = System.Drawing.Color.Gainsboro;
+            this.btnCatalogs.TextFont = new System.Drawing.Font("Itim", 12F);
+            // 
             // btnHome
             // 
-            this.btnHome.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(92)))), ((int)(((byte)(92)))));
-            this.btnHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnHome.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(125)))), ((int)(((byte)(154)))));
+            this.btnHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(92)))), ((int)(((byte)(92)))));
             this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnHome.BorderRadius = 0;
             this.btnHome.ButtonText = "Home";
@@ -126,7 +167,7 @@
             this.btnHome.TabIndex = 5;
             this.btnHome.Text = "Home";
             this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnHome.Textcolor = System.Drawing.Color.White;
+            this.btnHome.Textcolor = System.Drawing.Color.Gainsboro;
             this.btnHome.TextFont = new System.Drawing.Font("Itim", 12F);
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
@@ -138,43 +179,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(253, 60);
             this.panel1.TabIndex = 4;
-            // 
-            // bunifuFlatButton1
-            // 
-            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(92)))), ((int)(((byte)(92)))));
-            this.bunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.bunifuFlatButton1.BorderRadius = 0;
-            this.bunifuFlatButton1.ButtonText = "Home";
-            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
-            this.bunifuFlatButton1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.bunifuFlatButton1.Font = new System.Drawing.Font("Itim", 12F);
-            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
-            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
-            this.bunifuFlatButton1.Iconimage_right = null;
-            this.bunifuFlatButton1.Iconimage_right_Selected = null;
-            this.bunifuFlatButton1.Iconimage_Selected = null;
-            this.bunifuFlatButton1.IconMarginLeft = 30;
-            this.bunifuFlatButton1.IconMarginRight = 0;
-            this.bunifuFlatButton1.IconRightVisible = true;
-            this.bunifuFlatButton1.IconRightZoom = 0D;
-            this.bunifuFlatButton1.IconVisible = true;
-            this.bunifuFlatButton1.IconZoom = 50D;
-            this.bunifuFlatButton1.IsTab = true;
-            this.bunifuFlatButton1.Location = new System.Drawing.Point(0, 107);
-            this.bunifuFlatButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
-            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
-            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
-            this.bunifuFlatButton1.selected = false;
-            this.bunifuFlatButton1.Size = new System.Drawing.Size(253, 47);
-            this.bunifuFlatButton1.TabIndex = 6;
-            this.bunifuFlatButton1.Text = "Home";
-            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
-            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Itim", 12F);
             // 
             // bunifuCustomLabel1
             // 
@@ -188,6 +192,80 @@
             this.bunifuCustomLabel1.Size = new System.Drawing.Size(64, 24);
             this.bunifuCustomLabel1.TabIndex = 2;
             this.bunifuCustomLabel1.Text = "MENU";
+            // 
+            // btnUpdates
+            // 
+            this.btnUpdates.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(125)))), ((int)(((byte)(154)))));
+            this.btnUpdates.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnUpdates.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUpdates.BorderRadius = 0;
+            this.btnUpdates.ButtonText = "Updates";
+            this.btnUpdates.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdates.DisabledColor = System.Drawing.Color.Gray;
+            this.btnUpdates.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUpdates.Font = new System.Drawing.Font("Itim", 12F);
+            this.btnUpdates.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnUpdates.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnUpdates.Iconimage")));
+            this.btnUpdates.Iconimage_right = null;
+            this.btnUpdates.Iconimage_right_Selected = null;
+            this.btnUpdates.Iconimage_Selected = null;
+            this.btnUpdates.IconMarginLeft = 30;
+            this.btnUpdates.IconMarginRight = 0;
+            this.btnUpdates.IconRightVisible = true;
+            this.btnUpdates.IconRightZoom = 0D;
+            this.btnUpdates.IconVisible = true;
+            this.btnUpdates.IconZoom = 50D;
+            this.btnUpdates.IsTab = true;
+            this.btnUpdates.Location = new System.Drawing.Point(0, 154);
+            this.btnUpdates.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnUpdates.Name = "btnUpdates";
+            this.btnUpdates.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnUpdates.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.btnUpdates.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnUpdates.selected = false;
+            this.btnUpdates.Size = new System.Drawing.Size(253, 47);
+            this.btnUpdates.TabIndex = 7;
+            this.btnUpdates.Text = "Updates";
+            this.btnUpdates.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnUpdates.Textcolor = System.Drawing.Color.Gainsboro;
+            this.btnUpdates.TextFont = new System.Drawing.Font("Itim", 12F);
+            // 
+            // btnFavorites
+            // 
+            this.btnFavorites.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(125)))), ((int)(((byte)(154)))));
+            this.btnFavorites.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnFavorites.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFavorites.BorderRadius = 0;
+            this.btnFavorites.ButtonText = "Favorites";
+            this.btnFavorites.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFavorites.DisabledColor = System.Drawing.Color.Gray;
+            this.btnFavorites.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFavorites.Font = new System.Drawing.Font("Itim", 12F);
+            this.btnFavorites.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnFavorites.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnFavorites.Iconimage")));
+            this.btnFavorites.Iconimage_right = null;
+            this.btnFavorites.Iconimage_right_Selected = null;
+            this.btnFavorites.Iconimage_Selected = null;
+            this.btnFavorites.IconMarginLeft = 30;
+            this.btnFavorites.IconMarginRight = 0;
+            this.btnFavorites.IconRightVisible = true;
+            this.btnFavorites.IconRightZoom = 0D;
+            this.btnFavorites.IconVisible = true;
+            this.btnFavorites.IconZoom = 50D;
+            this.btnFavorites.IsTab = true;
+            this.btnFavorites.Location = new System.Drawing.Point(0, 201);
+            this.btnFavorites.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnFavorites.Name = "btnFavorites";
+            this.btnFavorites.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnFavorites.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.btnFavorites.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnFavorites.selected = false;
+            this.btnFavorites.Size = new System.Drawing.Size(253, 47);
+            this.btnFavorites.TabIndex = 8;
+            this.btnFavorites.Text = "Favorites";
+            this.btnFavorites.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnFavorites.Textcolor = System.Drawing.Color.Gainsboro;
+            this.btnFavorites.TextFont = new System.Drawing.Font("Itim", 12F);
             // 
             // MangaHonKoFrom
             // 
@@ -218,8 +296,10 @@
         private System.Windows.Forms.Panel panelLeft;
         private System.Windows.Forms.Panel panel1;
         private Bunifu.Framework.UI.BunifuFlatButton btnHome;
-        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private Bunifu.Framework.UI.BunifuFlatButton btnCatalogs;
         private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuFlatButton btnFavorites;
+        private Bunifu.Framework.UI.BunifuFlatButton btnUpdates;
     }
 }
 
